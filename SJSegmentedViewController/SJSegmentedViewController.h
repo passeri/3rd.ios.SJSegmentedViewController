74ce433027e3ee2bce9930e9ab014ed57aebecb8
tomita-toshihiro <tomita@passeri.jp>
2017-07-11 01:05:43 +0900

    Initial Commit

---
+++ SJSegmentedViewController/SJSegmentedViewController.h
@@ -0,0 +1,19 @@
+//
+//  SJSegmentedViewController.h
+//  SJSegmentedViewController
+//
+//  Created by 冨田 俊大 on 2017/07/11.
+//  Copyright © 2017年 Passeri inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SJSegmentedViewController.
+FOUNDATION_EXPORT double SJSegmentedViewControllerVersionNumber;
+
+//! Project version string for SJSegmentedViewController.
+FOUNDATION_EXPORT const unsigned char SJSegmentedViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJSegmentedViewController/PublicHeader.h>
+
+
